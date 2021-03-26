0c79438d1ee1a493c19cfbcab482b7b355d418af
tilpner <till@hoeppner.ws>
2021-03-26 19:30:46 +0100

    vdev: convert error type from error-chain to thiserror
    This is necessary for Sync errors, which is a requirement of the storage
    pool layer.

--- betree/src/vdev/errors.rs
+++ betree/src/vdev/errors.rs
@@ -1,21 +1,35 @@
 #![allow(missing_docs, unused_doc_comments)]
-error_chain! {
-    types {
-        Error, ErrorKind, ResultExt;
-    }
-    foreign_links {
-        Io(::std::io::Error);
-        ChecksumError(crate::checksum::ChecksumError);
-    }
-    errors {
-        ReadError(id: String)
-        WriteError(id: String)
-        SpawnError(error: String)
+
+use std::sync::Arc;
+
+#[derive(thiserror::Error, Debug, Clone)]
+pub enum VdevError {
+    #[error("Io: {0}")]
+    Io(Arc<std::io::Error>),
+    #[error("Checksum: {0}")]
+    Checksum(Arc<crate::checksum::ChecksumError>),
+    #[error("Read: id={0}")]
+    Read(String),
+    #[error("Write: id={0}")]
+    Write(String),
+    #[error("Spawn: {0}")]
+    Spawn(Arc<futures::task::SpawnError>),
+}
+
+impl From<std::io::Error> for VdevError {
+    fn from(io_err: std::io::Error) -> Self {
+        VdevError::Io(Arc::new(io_err))
     }
 }
 
-impl From<::futures::task::SpawnError> for Error {
-    fn from(e: ::futures::task::SpawnError) -> Error {
-        Error::from(ErrorKind::SpawnError(format!("{:?}", e)))
+impl From<crate::checksum::ChecksumError> for VdevError {
+    fn from(checksum_err: crate::checksum::ChecksumError) -> Self {
+        VdevError::Checksum(Arc::new(checksum_err))
+    }
+}
+
+impl From<futures::task::SpawnError> for VdevError {
+    fn from(spawn_err: futures::task::SpawnError) -> Self {
+        VdevError::Spawn(Arc::new(spawn_err))
     }
 }
