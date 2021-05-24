401a9531488818ecb71a70f1b6914bba113a6e04
tilpner <till@hoeppner.ws>
2021-05-24 10:21:53 +0200

    tree/packed: rewrite leaf packing to save more space, and hold keyinfo

--- betree/src/tree/imp/packed.rs
+++ betree/src/tree/imp/packed.rs
@@ -2,31 +2,53 @@ use super::leaf::LeafNode;
 use crate::{
     cow_bytes::{CowBytes, SlicedCowBytes},
     size::Size,
+    tree::KeyInfo,
+    StoragePreference,
 };
 use byteorder::{ByteOrder, LittleEndian, WriteBytesExt};
 use std::{
     cmp,
     io::{self, Write},
-    mem::{align_of, size_of},
-    slice::{from_raw_parts, from_raw_parts_mut},
+    mem::size_of,
 };
 
 // account for trailing fake element
-pub(crate) const HEADER_FIXED_LEN: usize = size_of::<Header>() + HEADER_PER_KEY_METADATA_LEN;
-pub(crate) const HEADER_PER_KEY_METADATA_LEN: usize = 2 * size_of::<Offset>();
+pub(crate) const HEADER_FIXED_LEN: usize = HEADER_LEN + OFFSET_LEN;
+
+const HEADER_LEN: usize = size_of::<u32>();
+
+// Offsets are stored as 24-bit unsigned integers in little-endian order
+pub(crate) const OFFSET_LEN: usize = 3;
+// 2 offsets (u24) and a keyinfo (u8)
+pub(crate) const ENTRY_LEN: usize = 2 * OFFSET_LEN + 1;
+pub(crate) const ENTRY_KEY_OFFSET: usize = 0;
+pub(crate) const ENTRY_KEY_INFO_OFFSET: usize = ENTRY_KEY_OFFSET + OFFSET_LEN;
+pub(crate) const ENTRY_DATA_OFFSET: usize = ENTRY_KEY_INFO_OFFSET + 1;
 
 /// ```text
 /// Layout:
-///     header: Header,
-///     # We compute the length by subtracting two consecutive offsets,
-///     # this requires a trailing fake offset to compute the length of the last actual element.
-///     keys: [Offset; header.entry_count + 1],
-///     values: [Offset; header.entry_count + 1],
-///     data: [u8],
-/// Header:
 ///     entry_count: u32,
+///     entries: [Entry; header.entry_count],
+///     # necessary to compute the length of the last value
+///     data_end: Offset,
+///     data: [u8]
+///
+/// # These positions only mark the beginning of the key and value section in data,
+/// # the length is computed by subtracting two consecutive offsets.
+/// # This is possible because entries are written in the same order as their data
+/// # will have in `data`.
+/// Entry:
+///     key_pos: Offset,
+///     key_info: KeyInfo,
+///     data_pos: Offset
+///
+/// # 2^24 byte ~= 16.7MB, plenty at a max node size of 4MiB
 /// Offset:
-///     u32
+///     u24
+///
+/// KeyInfo:
+///     storage_preference: u8
+///
 /// ```
 #[derive(Debug)]
 pub struct PackedMap {
@@ -35,64 +57,69 @@ pub struct PackedMap {
 }
 
 #[derive(Debug, Copy, Clone)]
-#[repr(packed)]
-struct Header {
-    entry_count: u32,
-}
-
-#[derive(Debug, Copy, Clone)]
-#[repr(packed)]
 struct Offset(u32);
 
-fn ref_u32_from_bytes_mut(p: &mut [u8]) -> &mut [u32] {
-    assert!(p.len() % size_of::<u32>() == 0);
-    assert!(p.as_ptr() as usize % align_of::<[u32; 1]>() == 0);
-    unsafe { from_raw_parts_mut(p.as_mut_ptr() as *mut u32, p.len() / 4) }
-}
-
 fn prefix_size(entry_count: u32) -> usize {
-    size_of::<Header>() + HEADER_PER_KEY_METADATA_LEN * (entry_count + 1) as usize
+    HEADER_FIXED_LEN + ENTRY_LEN * entry_count as usize
 }
 
 impl PackedMap {
-    pub fn new(mut data: Vec<u8>) -> Self {
+    pub fn new(data: Vec<u8>) -> Self {
         assert!(data.len() >= 4);
         let entry_count = LittleEndian::read_u32(&data[..4]);
-        let total_bytes = prefix_size(entry_count);
-        {
-            let s = ref_u32_from_bytes_mut(&mut data[..total_bytes]);
-            LittleEndian::from_slice_u32(s);
-        }
+
         PackedMap {
             data: data.into(),
             entry_count,
         }
     }
 
-    /// Returns (position, len) for the metadata entry at position `m_idx`.
-    /// If `m_idx < entry_count`, it will return the metadata of a value,
-    /// if `m_idx == entry_count`, undefined nonsense will be returned,
-    /// if `m_idx > entry_count`, the metadata of the value at position `m_idx - entry_count - 1` will
-    /// be returned.
-    fn position(&self, m_idx: u32) -> (Offset, u32) {
-        let m_idx = m_idx as usize;
-        let positions = unsafe {
-            let ptr = self.data.as_ptr();
-            let start = ptr.add(size_of::<Header>()) as *const Offset;
-            from_raw_parts(start, (self.entry_count as usize + 1) * 2)
-        };
-
-        (
-            positions[m_idx],
-            positions[m_idx + 1].0 - positions[m_idx].0,
-        )
+    fn read_offset(&self, byte_idx: usize) -> Offset {
+        Offset(LittleEndian::read_u24(
+            &self.data[byte_idx..byte_idx + OFFSET_LEN],
+        ))
     }
 
+    // In the data segment, the value is always written directly after the key,
+    // so the key length can be calculated by subtraction.
     fn key_pos(&self, idx: u32) -> (Offset, u32) {
-        self.position(idx)
+        assert!(idx < self.entry_count);
+
+        let entry_pos = HEADER_LEN + idx as usize * ENTRY_LEN;
+
+        let key_offset = self.read_offset(entry_pos + ENTRY_KEY_OFFSET);
+        let data_offset = self.read_offset(entry_pos + ENTRY_DATA_OFFSET);
+        let key_len = data_offset.0 - key_offset.0;
+
+        (key_offset, key_len)
     }
+
+    // In the data segment, the next key is usually written after the current value,
+    // so the value length can usually be calculated by subtraction.
     fn val_pos(&self, idx: u32) -> (Offset, u32) {
-        self.position(self.entry_count + 1 + idx)
+        assert!(idx < self.entry_count);
+
+        let entry_pos = HEADER_LEN + idx as usize * ENTRY_LEN;
+        let data_offset = self.read_offset(entry_pos + ENTRY_DATA_OFFSET);
+
+        // this works even for the last entry, as a single offset is appended to the last full
+        // entry, and the key offset comes first, so the rest of that fake entry is not missed.
+        let next_entry_pos = entry_pos + ENTRY_LEN;
+        let next_key_offset = self.read_offset(next_entry_pos + ENTRY_KEY_OFFSET);
+
+        let data_len = next_key_offset.0 - data_offset.0;
+        (data_offset, data_len)
+    }
+
+    fn key_info(&self, idx: u32) -> KeyInfo {
+        debug_assert!(idx < self.entry_count);
+        let entry_pos = HEADER_LEN + idx as usize * ENTRY_LEN;
+
+        KeyInfo {
+            storage_preference: StoragePreference::from_u8(
+                self.data[entry_pos + ENTRY_KEY_INFO_OFFSET],
+            ),
+        }
     }
 
     fn get_slice(&self, (Offset(pos), len): (Offset, u32)) -> &[u8] {
@@ -126,29 +153,34 @@ impl PackedMap {
         }
     }
 
-    pub fn get(&self, key: &[u8]) -> Option<SlicedCowBytes> {
+    pub fn get(&self, key: &[u8]) -> Option<(KeyInfo, SlicedCowBytes)> {
         let result = self.binary_search(key);
         let idx = match result {
             Err(_) => return None,
             Ok(idx) => idx,
         };
 
-        Some(self.get_slice_cow(self.val_pos(idx)))
+        Some((self.key_info(idx), self.get_slice_cow(self.val_pos(idx))))
     }
 
-    pub fn get_all<'a>(&'a self) -> impl Iterator<Item = (&'a [u8], SlicedCowBytes)> + 'a {
+    pub fn get_all<'a>(
+        &'a self,
+    ) -> impl Iterator<Item = (&'a [u8], (KeyInfo, SlicedCowBytes))> + 'a {
         struct Iter<'a> {
             packed: &'a PackedMap,
             idx: u32,
         }
         impl<'a> Iterator for Iter<'a> {
-            type Item = (&'a [u8], SlicedCowBytes);
+            type Item = (&'a [u8], (KeyInfo, SlicedCowBytes));
 
             fn next(&mut self) -> Option<Self::Item> {
                 if self.idx < self.packed.entry_count {
                     let ret = Some((
                         self.packed.get_slice(self.packed.key_pos(self.idx)),
-                        self.packed.get_slice_cow(self.packed.val_pos(self.idx)),
+                        (
+                            self.packed.key_info(self.idx),
+                            self.packed.get_slice_cow(self.packed.val_pos(self.idx)),
+                        ),
                     ));
                     self.idx += 1;
                     ret
@@ -172,25 +204,22 @@ impl PackedMap {
         let entries = leaf.entries();
         let entries_cnt = entries.len() as u32;
         writer.write_u32::<LittleEndian>(entries_cnt)?;
+
         let mut pos = prefix_size(entries_cnt) as u32;
-        for key in entries.keys() {
-            writer.write_u32::<LittleEndian>(pos)?;
-            let len = key.len() as u32;
-            pos += len;
-        }
-        writer.write_u32::<LittleEndian>(pos)?;
+        for (key, (keyinfo, value)) in entries {
+            writer.write_u24::<LittleEndian>(pos)?;
+            pos += key.len() as u32;
 
-        for value in entries.values() {
-            writer.write_u32::<LittleEndian>(pos)?;
-            let len = value.len() as u32;
-            pos += len;
-        }
-        writer.write_u32::<LittleEndian>(pos)?;
+            writer.write_u8(keyinfo.storage_preference.as_u8())?;
 
-        for key in entries.keys() {
+            writer.write_u24::<LittleEndian>(pos)?;
+            pos += value.len() as u32;
+        }
+
+        writer.write_u24::<LittleEndian>(pos)?;
+
+        for (key, (_keyinfo, value)) in entries {
             writer.write_all(key)?;
-        }
-        for value in entries.values() {
             writer.write_all(value)?;
         }
         Ok(())
@@ -218,8 +247,10 @@ mod tests {
 
         let packed = PackedMap::new(v);
 
-        for (k, v) in leaf.entries() {
-            assert_eq!(Some(v), packed.get(k).as_ref());
+        for (k, (ki, v)) in leaf.entries() {
+            let (pki, pv) = packed.get(k).unwrap();
+            assert_eq!(ki, &pki, "keyinfo mismatch");
+            assert_eq!(v, &pv, "value mismatch");
         }
 
         assert_eq!(
