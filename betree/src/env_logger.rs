9d72fdb3a23e2525dd1a67303c05551daf0c30b9
tilpner <till@hoeppner.ws>
2021-05-24 16:21:43 +0200

    env_logger: document

--- betree/src/env_logger.rs
+++ betree/src/env_logger.rs
@@ -1,3 +1,8 @@
+//! Default `env_logger` configuration.
+
+/// Initialise `env_logger` with a modified configuration.
+/// This function exists for logging format consistency between multiple embedding
+/// applications/libraries.
 pub fn init_env_logger() {
     env_logger::Builder::from_default_env()
         .format_timestamp_millis()
