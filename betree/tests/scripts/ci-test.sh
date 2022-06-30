7adcb478ea9bcc5cb224919866a72b3cca40e8db
Johannes Wünsche <johannes@spacesnek.rocks>
2022-06-30 15:45:43 +0200

    ci: output log on stderr

--- betree/tests/scripts/ci-test.sh
+++ betree/tests/scripts/ci-test.sh
@@ -1,5 +1,5 @@
 #! /bin/env bash
 
 # num_thread=$(echo "$(cat /proc/meminfo | head -n 1 | xargs | cut -d ' ' -f 2) / 1024 / 1024 / 2" | bc)
-failed=$(cargo test -- --test-threads 1 -Z unstable-options --format json | grep suite | grep failed | jq '.failed')
+failed=$(cargo test -- --test-threads 1 -Z unstable-options --format json | tee /dev/stderr | grep suite | grep failed | jq '.failed')
 exit $failed
