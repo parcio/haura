3f7793eb83296a685fc2de4b7cc5825a19255f2a
Johannes Wünsche <johannes@spacesnek.rocks>
2022-07-14 07:58:33 +0000

    Add CI for `betree` (#3)
    * ci: add pr workflow
    
    * ci: add trigger for version tags
    
    * ci: output log on stderr
    
    * ci: modify triggers
    
    For later it will be advantageous to have it simply run on the main on
    every push as we're still in dev.
    
    * ci: add separate test for internal test and integration
    
    * ci: add test script betree internal
    
    * ci: reformat test report
    
    * ci: fix exit code for jobs
    
    * ci: remove debug output
    
    * ci: add sharedkey for build cache
    
    * ci: fix target path
    
    * ci: remove serial tests for betree internal
    
    * ci: fix target path
    
    * ci: add backtrace to env
    
    * ci: replace Swatinem/rust-cache with actions/cache
    
    * ci: replace actions-rs/toolchain with simple script
    
    * ci: eliminate relative path to target

---
+++ betree/tests/scripts/ci-test.sh
@@ -0,0 +1,20 @@
+#! /bin/env bash
+
+# num_thread=$(echo "$(cat /proc/meminfo | head -n 1 | xargs | cut -d ' ' -f 2) / 1024 / 1024 / 2" | bc)
+failed=$(cargo test -- --test-threads 1 -Z unstable-options --format json \
+    | grep name \
+    | grep failed \
+    | jq '"Test: " + .name + "\n-----LOG-----\n" + .stdout + "---END LOG---\n" ' \
+      )
+echo "FAILED TESTS"
+echo "############"
+for tst in "${failed[@]}"
+do
+    printf '%b' "$(echo "$tst" | sed -e 's/\"//g')"
+done
+if [ -z "$failed" ]
+then
+        exit 0
+else
+        exit 1
+fi
