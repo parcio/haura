86cc2cab2da042d48ae6011f84fecc3a8ff46e9a
Johannes Wünsche <johannes@spacesnek.rocks>
2022-07-30 14:10:31 +0200

    tests: handle file backed config with locking
    This allows for multiple tests to rely on the same files without
    additional checking or depedencies between tests. Problematic add the
    moment is that some tests are order dependent due to the storage not
    being nulled. Reading some data results in _some_ cases in a checksum
    error. This happens on either file mode, indicating some problem
    which has not been discovered yet.

--- betree/tests/src/configs.rs
+++ betree/tests/src/configs.rs
@@ -1,3 +1,8 @@
+use std::{
+    os::unix::prelude::FileExt,
+    sync::{Arc, Once, RwLock, RwLockWriteGuard},
+};
+
 use betree_storage_stack::{
     database::AccessMode,
     migration::{MigrationConfig, MigrationPolicies},
@@ -27,20 +32,35 @@ pub(crate) fn migration_config() -> DatabaseConfiguration {
     }
 }
 
-pub(crate) fn migration_config_file_backed() -> DatabaseConfiguration {
-    DatabaseConfiguration {
-        storage: StoragePoolConfiguration {
-            tiers: vec![TierConfiguration {
-                top_level_vdevs: vec![Vdev::Leaf(LeafVdev::File("test_disk_tier_fastest".into()))],
-            }],
+static mut FILE_BACKED_CONFIG: Option<RwLock<DatabaseConfiguration>> = None;
+
+static FILE_BACKED_CONFIG_INIT: Once = Once::new();
+
+// NOTE: A bit hacky solution to get somehting like a singleton working for limited resources
+//       like files in this case. Arguably still error-prone with the combination of the file_backed_config
+//       as a fixture the guard is kept alive and guarantees.
+pub(crate) fn migration_config_file_backed() -> RwLockWriteGuard<'static, DatabaseConfiguration> {
+    FILE_BACKED_CONFIG_INIT.call_once(|| unsafe {
+        FILE_BACKED_CONFIG = Some(RwLock::new(DatabaseConfiguration {
+            storage: StoragePoolConfiguration {
+                tiers: vec![TierConfiguration {
+                    top_level_vdevs: vec![Vdev::Leaf(LeafVdev::File(
+                        "test_disk_tier_fastest".into(),
+                    ))],
+                }],
+                ..Default::default()
+            },
+            migration_policy: Some(MigrationPolicies::Lfu(MigrationConfig {
+                grace_period: std::time::Duration::from_millis(0),
+                migration_threshold: 0.7,
+                update_period: std::time::Duration::from_millis(100),
+            })),
+            access_mode: AccessMode::AlwaysCreateNew,
             ..Default::default()
-        },
-        migration_policy: Some(MigrationPolicies::Lfu(MigrationConfig {
-            grace_period: std::time::Duration::from_millis(0),
-            migration_threshold: 0.7,
-            update_period: std::time::Duration::from_millis(100),
-        })),
-        access_mode: AccessMode::OpenOrCreate,
-        ..Default::default()
+        }));
+    });
+    unsafe {
+        let guard = FILE_BACKED_CONFIG.as_ref().unwrap();
+        guard.write().unwrap()
     }
 }
