6859766f3f16be1e6c1bcf9ce2fab30e74550ead
Johannes Wünsche <johannes@spacesnek.rocks>
2022-08-01 14:10:07 +0200

    migration: rework lfu

--- betree/src/migration/errors.rs
+++ betree/src/migration/errors.rs
@@ -1,5 +1,8 @@
 #![allow(missing_docs, unused_doc_comments)]
 error_chain! {
+    foreign_links {
+        DmuError(crate::data_management::errors::Error);
+    }
     errors {
         ConstructionFailed
         MigrationFailed
