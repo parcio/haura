e66ab53275f5ee81315b17515e8c086c5518a0e3
Johannes Wünsche <johannes@spacesnek.rocks>
2022-08-26 17:43:52 +0200

    object: fix range iter objects

--- betree/src/object/mod.rs
+++ betree/src/object/mod.rs
@@ -332,7 +332,7 @@ impl<'os, Config: DatabaseBuilder + Clone> ObjectStore<Config> {
     /// Return an iterator overall object names and metadata in this object store.
     pub fn iter_objects(&self) -> Result<impl Iterator<Item = (CowBytes, ObjectInfo)>> {
         // Iterate over the metadata and create tuples of object keys and ids.
-        Ok(self.metadata.range(&[0u8] as &[_]..&[u8::MAX] as &[_])?.map(|res| {
+        Ok(self.metadata.range(&[0u8] as &[_]..=&[u8::MAX] as &[_])?.map(|res| {
             let (k,v) = res.unwrap();
             (k, ObjectInfo::read_from_buffer_with_ctx(meta::ENDIAN, &v).unwrap())
         }))
