73ea02d5a5d68f056ebe4b745f5b07fe103da575
Johannes Wünsche <johannes@spacesnek.rocks>
2022-11-29 19:48:34 +0100

    julea-betree: fix build call (#21)

--- julea-betree/src/lib.rs
+++ julea-betree/src/lib.rs
@@ -93,7 +93,7 @@ unsafe extern "C" fn backend_init(path: *const gchar, backend_data: *mut gpointe
         let file = File::open(&path)?;
         let config: DatabaseConfiguration = serde_json::from_reader(&file)?;
 
-        let db = Database::build(config)?.with_sync();
+        let db = Database::build_threaded(config)?;
 
         Ok(Backend {
             database: db,
