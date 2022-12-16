d14cfaca63dca4e476b306b565697263433dc38d
Johannes Wünsche <johannes@spacesnek.rocks>
2022-12-16 18:02:13 +0100

    tests: simplify ci script

--- betree/tests/scripts/ci-test.sh
+++ betree/tests/scripts/ci-test.sh
@@ -2,35 +2,14 @@
 
 set -e
 
-# execute separately to avoid swallowing return code
-cargo build --tests
-
 if [ -e scripts/prepare-test.sh ]
 then
     ./scripts/prepare-test.sh
 fi
 
-failed=$(cargo test -- --test-threads "${HAURA_NUM_THREAD}" -Z unstable-options --format json \
-    | grep name \
-    | grep failed \
-    | jq '"Test: " + .name + "\n-----LOG-----\n" + .stdout + "---END LOG---\n" ' \
-      )
-# Reset fail.log to empty if already present
-echo "FAILED TESTS" > fail.log
-echo "############" >> fail.log
-for tst in "${failed[@]}"
-do
-    printf '%b' "$(echo "$tst" | sed -e 's/\"//g')" >> fail.log
-done
+cargo test -- --test-threads "${HAURA_NUM_THREAD}"
 
 if [ -e scripts/cleanup-test.sh ]
 then
     ./scripts/cleanup-test.sh
-fi
-
-if [ -z "$failed" ]
-then
-    exit 0
-else
-    exit 1
 fi
