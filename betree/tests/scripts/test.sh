0532aad2f917aed63aab542f3ddaf1490884523e
Johannes Wünsche <johannes@spacesnek.rocks>
2022-12-17 14:25:50 +0100

    Improve test scripts (#30)
    * tests: build tests preemptive to detect sensible errors
    
    * tests: ensure post cleanup
    
    * tests: improve num_thread determination
    
    * tests: crate resources only when required
    
    * tests: fix append to log
    
    * tests: simplify ci script

--- betree/tests/scripts/test.sh
+++ betree/tests/scripts/test.sh
@@ -2,11 +2,18 @@
 
 ./scripts/prepare-test.sh
 
-num_thread=$(echo "$(grep 'MemFree' /proc/meminfo | xargs | cut -d ' ' -f 2) / 1024 / 1024 / 5" | bc)
+num_thread=$(echo "$(grep 'MemAvailable' /proc/meminfo | xargs | cut -d ' ' -f 2) / 1024 / 1024 / 4" | bc)
 
 if [ "$num_thread" -gt "$(nproc)" ]
 then
     num_thread=$(nproc)
+fi
+
+if [ "$num_thread" = 0 ]
+then
+    echo "Could not determine enough memory to run tests."
+    echo "Trying with single thread..."
+    num_thread=1
 fi
 
 echo "Using ${num_thread} threads."
