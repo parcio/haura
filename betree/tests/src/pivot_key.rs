5cb58ea2074fe6ae07e4ce49e8101af8eb310fb2
fia <fia@spacesnek.rocks>
2024-02-01 16:43:39 +0100

    tests: increase test db size for pivot key
    Internal fragmentation made this necessary with the smaller cache size for key-value store tests.

--- betree/tests/src/pivot_key.rs
+++ betree/tests/src/pivot_key.rs
@@ -4,14 +4,14 @@ use rand::seq::IteratorRandom;
 
 #[test]
 fn structure_is_good() {
-    let (_db, ds) = util::random_db(2, 128, StorageKind::Block);
+    let (_db, ds) = util::random_db(1, 256, StorageKind::Block);
     let dmp = ds.tree_dump().unwrap();
     internal_node_check(&dmp)
 }
 
 #[test]
 fn get() {
-    let (_db, ds) = util::random_db(2, 128, StorageKind::Block);
+    let (db, ds) = util::random_db(1, 256, StorageKind::Block);
     let dmp = ds.tree_dump().unwrap();
     let pk = random_pivot_key(&dmp).unwrap();
     let _node = ds.test_get_node_pivot(pk).unwrap().unwrap();
